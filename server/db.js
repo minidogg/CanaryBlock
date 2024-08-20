0f43eb99f212ee12530784108f0c5c4328ee7bcb
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-08-19 22:03:32 -0500

    making a commit before i forget and break everything while trying to optimize

--- server/db.js
+++ server/db.js
@@ -7,13 +7,110 @@ import * as fu from './file.js';
 
 // Generate database related paths
 const dbPath = path.resolve("../db");
-const blockedPath = path.join(dbPath, "/blocked.cbdb")
-const whitelistPath = path.join(dbPath, "/whitelist.cbdb")
+const blockedPath = path.join(dbPath, "/blocked.cbdb");
+const whitelistPath = path.join(dbPath, "/whitelist.cbdb");
 
 // Ensure database files and directories are generated.
-fu.TryMkDir(dbPath)
-fu.TryMkFile(blockedPath, "", "utf-8")
-fu.TryMkFile(whitelistPath, "", "utf-8")
+fu.TryMkDir(dbPath);
+fu.TryMkFile(blockedPath, "", "utf-8");
+fu.TryMkFile(whitelistPath, "", "utf-8");
 
+// Cache arrays to minimize reads
+let blockedCache = [];
+let whitelistCache = [];
+
+// Chunk Size
+const CHUNK_SIZE = 1;
+
+// General Finding Function
+async function DBFind(filePath, condition) {
+    const stream = fs.createReadStream(filePath, {
+        highWaterMark: CHUNK_SIZE
+    });
+    let text = ""
+    let found = false
+
+    for await (const data of stream) {
+        text += data.toString()
+        if (text.endsWith("\n")) {
+            text = text.replace("\n", "")
+            text = text.replace("\r", "")
+
+
+            if (condition(text) == true) {
+                found = true
+                break
+            }
+
+            text = ""
+
+        }
+    }
+
+    return found
+}
+
+function SliceArrayOverflow(array, max = 50){
+    if(!isCacheChecking && array.length>=max)array.pop()
+}
+
+// TODO: Name this variable better
+async function SillyFinder9000(filePath, cache, url){
+    for(let i of cache){
+        if(typeof(i=="string"&&url.startsWith(i))){
+            return true;
+        }
+        if(i instanceof RegExp&&i.test(url)){
+            return true;
+        }
+    }
+
+
+    let foundCondition = function(text){
+        try{
+            if(url.startsWith(text)){
+                cache.push(text)
+                SliceArrayOverflow(cache)
+                return true;
+            }
+            let reg = new RegExp(text);
+
+            if(reg.test(url)){
+                cache.push(reg)
+                SliceArrayOverflow(cache)
+                return true;
+            }
+        }catch{}
+        return false;
+    }
+
+    
+    
+    return DBFind(filePath, foundCondition)
+}
 
 // Function for checking if a url is blocked
+async function IsBlocked(url){
+    if(IsWhitelisted(url)==true)return false;
+    return SillyFinder9000(blockedPath, blockedCache, url)
+}
+
+
+// Function for checking if a url is whitelisted
+async function IsWhitelisted(url){
+    return SillyFinder9000(whitelistPath, whitelistCache, url)
+}
+
+console.time("No Cache")
+console.log(await IsBlocked("https://google.com/addsasads")) // False
+// console.log(await IsWhitelisted("https://google.com/dsasads")) // True
+console.log(await IsBlocked("https://sites.google.com/site/classroom6x/the-final-earth-2?authuser=0")) // True
+// console.log(await IsWhitelisted("https://sites.google.com/site/classroom6x/the-final-earth-2?authuser=0")) // False
+console.timeEnd("No Cache")
+
+console.time("Cache")
+console.log(await IsBlocked("https://google.com/addsasads")) // False
+// console.log(await IsWhitelisted("https://google.com/dsasads")) // True
+console.log(await IsBlocked("https://sites.google.com/site/classroom6x/the-final-earth-2?authuser=0")) // True
+// console.log(await IsWhitelisted("https://sites.google.com/site/classroom6x/the-final-earth-2?authuser=0")) // False
+console.timeEnd("Cache")
--- server/limex.js
+++ server/limex.js
@@ -1,3 +1,5 @@
+// TODO: Make a simplified RegExp engine for better performance (hopefully)
+
 export function escapeRegex(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // Escape all special characters
 }
